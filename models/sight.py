d6a84225096008a3bc718b7e82392b82106b447a
Georgi <ggeorgeuk@gmail.com>
2022-10-12 16:39:28 +0100

    Add files via upload

---
+++ models/sight.py
@@ -0,0 +1,8 @@
+class Sight():
+    def __init__(self, name, city, visit = False, id = None):
+        self.name = name
+        self.city = city
+        self.visit = visit
+        self.id = id
+
+    
