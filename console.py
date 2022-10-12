b6eae45ab96bd74fc16e8b9f7ada759cdbeadda5
Georgi <ggeorgeuk@gmail.com>
2022-10-12 16:27:36 +0100

    Add files via upload

---
+++ console.py
@@ -0,0 +1,15 @@
+
+import pdb
+from models.country import Country
+from models.city import City
+from models.sight import Sight
+
+import repositories.country_repository as country_repository
+import repositories.city_repository as city_repository
+import repositories.sight_repository as sight_repository
+
+
+#create python obj and save it(invoke repository functionality)
+#orders matters
+
+pdb.set_trace()
