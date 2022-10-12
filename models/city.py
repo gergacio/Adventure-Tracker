e9823201b89e5f3d22c48f13767a15d00a73b9ca
Georgi <ggeorgeuk@gmail.com>
2022-10-12 16:38:29 +0100

    Add files via upload

---
+++ models/city.py
@@ -0,0 +1,9 @@
+class City():
+    def __init__(self, name, country, visit = False, id = None):
+        self.name = name 
+        self.country = country
+        self.visit = visit
+        self.id = id
+        self.sights = []
+
+      
